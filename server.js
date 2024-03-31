df2503b835d86dc53614d13a0f6c0447ce948b7e
HaythamAli <haythampop50@gmail.com>
2024-03-31 17:05:44 +0200

    upDateCore

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: ["*"],
-    credentials: true,
-  })
-);
+app.use(cors());
 const userRoute = require("./routes/userRoute");
 const profileRoute = require("./routes/profileRoute");
 const postRoute = require("./routes/postRoute");
