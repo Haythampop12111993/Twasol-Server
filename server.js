0df8e4701331acdfb62840805287d8c699878232
HaythamAli <haythampop50@gmail.com>
2024-03-31 17:16:01 +0200

    upDateCors

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200"); // Allow all origins
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:4200"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify the methods you want to allow
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify the headers you want to allow
+  })
+);
 const userRoute = require("./routes/userRoute");
 const profileRoute = require("./routes/profileRoute");
 const postRoute = require("./routes/postRoute");
