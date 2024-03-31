bd8cb4ba596afc55e13668570a2b4edbe75c465b
HaythamAli <haythampop50@gmail.com>
2024-03-31 13:33:33 +0200

    upDateCors

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:4200",
-    credentials: true, // allow session cookies from browser to back-end server,
-    optionSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+    // origin: "http://localhost:4200",
+    // credentials: true, // allow session cookies from browser to back-end server,
+    // optionSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
   })
 );
 const userRoute = require("./routes/userRoute");
